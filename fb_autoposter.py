947f406860bddc6c13fa22c30e4c279f40a4045d
stzanetis <stzaneti@physics.auth.gr>
2019-03-14 20:00:52 +0200

    Create fb_autoposter.py

---
+++ fb_autoposter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import facebook
+
+def init_token(token):
+    cfg = {
+        "page_id"      : '',
+        "access_token" : token
+    }
+    return cfg
+
+def get_api(cfg):
+    graph = facebook.GraphAPI(access_token = cfg['access_token'], version='2.12', timeout=15)
+    print(graph)
+    resp = graph.get_object("me/accounts")
+    page_access_token = None
+    for page in resp['data']:
+        if page['id'] == cfg['page_id']:
+      	    page_access_token = page['access_token']
+    graph = facebook.GraphAPI(page_access_token)
+    return graph
+
+
+def post_msg(msg, api):
+    api.put_object('me', 'feed', message=msg)
+
